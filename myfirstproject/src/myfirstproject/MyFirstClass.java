cddccf3ab006528433bd4602e7a6f5941118630e
Asus <Asus@DESKTOP-2F2HHOK.station>
2019-04-16 17:11:20 +0200

    Prima Commit, Paolo

---
+++ myfirstproject/src/myfirstproject/MyFirstClass.java
@@ -0,0 +1,14 @@
+package myfirstproject;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("https://github.com/pfortino/MyFirstRepository.git");
+	
+	
+	}
+
+}
